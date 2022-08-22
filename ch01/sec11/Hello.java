112b3b9c5f91f91e0dbc138845996e45ab7783e5
Eunjong95 <111746153+Eunjong95@users.noreply.github.com>
2022-08-22 20:05:58 +0900

    Chapter1 실습코드

---
+++ ch01/sec11/Hello.java
@@ -0,0 +1,26 @@
+package ch01.sec11;
+
+public class Hello {
+	/**
+	 * @author KOSA
+	 * @param args
+	 */
+	
+    /*
+    장제목: 1장 자바시작하기
+    작성일: 2022.08.22
+     */
+	
+	/*
+	 * 장제목: 1장 자바시작하기
+	 * 작성일: 2022.08.22
+	 */
+	
+	//프로그램 실행 진입점. 메인 메소드
+	public static void main(String[] args) {
+
+      System.out.println("Hello, Java2");
+      
+	}
+
+}
