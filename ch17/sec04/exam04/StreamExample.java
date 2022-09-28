d8f3a5cb83566d749fb6d0f656b92ab4f9e2b212
Eunjong95 <111746153+Eunjong95@users.noreply.github.com>
2022-09-28 19:51:26 +0900

    Chapter17 실습코드

---
+++ ch17/sec04/exam04/StreamExample.java
@@ -0,0 +1,22 @@
+package ch17.sec04.exam04;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class StreamExample {
+
+	public static void main(String[] args) throws Exception {
+//		System.out.println(StreamExample.class);
+//		System.out.println(StreamExample.class.getResource("data.txt"));
+//		System.out.println(StreamExample.class.getResource("data.txt").toURI());
+		
+		Path path = Paths.get(StreamExample.class.getResource("data.txt").toURI());
+		Stream<String> stream = Files.lines(path, Charset.defaultCharset());
+		stream.forEach(line -> System.out.println(line));
+		stream.close();
+
+	}
+}
