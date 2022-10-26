b11f46fa23289e0971e7f4dffb99a12fbe4ea745
Eunjong95 <111746153+Eunjong95@users.noreply.github.com>
2022-10-26 21:21:48 +0900

    221026 project

--- project/JoinDAO.java
+++ project/JoinDAO.java
@@ -1,23 +1,17 @@
-package eunjong_join;
+package eunjongDAO;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+
+import eunjongDTO.JusersDTO;
 
 public class JoinDAO {
-	static Connection conn = null;
-
 	// 회원가입(JoinUser()-JuserDTO)
-	public static void JoinUser(JusersDTO jusersDTO) {
+	public static int JoinUser(JusersDTO jusersDTO, Connection conn) {
+		int rows = 0;
 		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@kosa402.iptime.org:50031:orcl", "younghun", "oracle");
-			System.out.println("연결 성공");
-
 			// users 컬럼값 입력
 			String sql = "INSERT INTO users(user_id, user_password, user_name, user_email, user_birth, user_gender, user_tel, user_address, user_money, user_point, user_date, user_delete)"
 					+ " VALUES(?, ?, ?, ?, ?, ?, ?, ?, 0, 0, sysdate, 'N')";
@@ -33,7 +27,7 @@ public class JoinDAO {
 			pstmt.setString(7, jusersDTO.getUser_tel());
 			pstmt.setString(8, jusersDTO.getUser_address());
 
-			pstmt.executeUpdate();
+			rows = pstmt.executeUpdate();
 			pstmt.close();
 
 		} catch (Exception e) {
@@ -47,20 +41,13 @@ public class JoinDAO {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("회원 가입 완료");
+		return rows;
 	}
 
 	// JoccupiedIdDAO :회원가입시 아이디 존재 여부에 대해 검사하는 메소드
-	public static boolean Juserid(String user_id) {
-		Connection conn = null;
+	public static boolean JUserid(String user_id, Connection conn) {
 		boolean occupiedId = false;
-
 		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@kosa402.iptime.org:50031:orcl", "younghun", "oracle");
-			System.out.println("연결 성공");
-
 			// users 테이블에 이미 있는 아이디인지 검사
 			String sql = "SELECT user_id From users WHERE user_id=?";
 
@@ -68,17 +55,8 @@ public class JoinDAO {
 			pstmt.setString(1, user_id);
 			ResultSet rs = pstmt.executeQuery();
 
-			ArrayList<String> jlist = new ArrayList<String>();
-			while (rs.next()) {
-				jlist.add(rs.getString("user_id"));
-			}
-
-			// 이미 있는 user_id면 true -> occupiedId
-			for (String a : jlist) {
-				if (user_id.equals(a)) {
-					occupiedId = true;
-					break;
-				}
+			if (rs.next()) {
+				occupiedId = true; // 행이 출력되면 이미 있는 아이디
 			}
 
 			rs.close();
@@ -97,5 +75,4 @@ public class JoinDAO {
 		}
 		return occupiedId;
 	}
-
 }
--- project/QnasDAO.java
+++ project/QnasDAO.java
@@ -1,27 +1,26 @@
-package eunjong_qnas;
+package eunjongDAO;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import eunjongDTO.QcreateDTO;
+import eunjongDTO.QdeleteDTO;
+import eunjongDTO.QnasDTO;
+import eunjongDTO.QreadDTO;
+import eunjongDTO.QupdateDTO;
+import eunjong_qnas.QviewcountDTO;
+
 public class QnasDAO {
-	static Connection conn = null;
-
 	// QNA 게시판 목록 조회 (QList())
-	public static void QList() {
+	public static ArrayList<QnasDTO> QList(Connection conn) {
+		ArrayList<QnasDTO> list = new ArrayList<>();
 		try {
-			// JDBC Driver 등록
-			Class.forName("oracle.jdbc.OracleDriver");
-			// 연결하기
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@kosa402.iptime.org:50031:orcl", "younghun", "oracle");
-			System.out.println("연결 성공");
-
 			// 글번호, 카테고리(문의유형), 아이디, 제목, 작성일, 조회수 띄우기
-			String sql = "SELECT qna_no, qna_category, user_id, qna_title, qna_date, qna_view" + " FROM qnas q, users u"
-					+ " WHERE q.user_id = u.user_id" + " ORDER BY qna_no DESC";
+			String sql = "SELECT qna_no, qna_category, q.user_id, qna_title, qna_date, qna_view"
+					+ " FROM qnas q, users u" + " WHERE q.user_id = u.user_id" + " ORDER BY qna_no DESC";
 
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
@@ -34,8 +33,7 @@ public class QnasDAO {
 				qnas.setQna_title(rs.getString("qna_title"));
 				qnas.setQna_date(rs.getDate("qna_date"));
 				qnas.setQna_view(rs.getInt("qna_view"));
-				System.out.printf("%-6s%-12s%-16s%-40s\n", qnas.getQna_no(), qnas.getQna_category(), qnas.getUser_id(),
-						qnas.getQna_title(), qnas.getQna_date(), qnas.getQna_view());
+				list.add(qnas);
 			}
 
 			rs.close();
@@ -52,28 +50,25 @@ public class QnasDAO {
 				e.printStackTrace();
 			}
 		}
+		return list;
 	}
 
 	// QNA 게시판 새 글 작성 (QCreate()-QcreateDTO)
-	public static void QCreate(QcreateDTO qcreateDTO) {
+	public static int QCreate(QcreateDTO qcreateDTO, Connection conn) {
+		int rows = 0;
 		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@kosa402.iptime.org:50031:orcl", "younghun", "oracle");
-			System.out.println("연결 성공");
-
-			// 글번호, 카테고리(문의유형), 아이디, 제목, 작성일, 조회수 띄우기
+			// 글번호, 카테고리(문의유형), 아이디, 제목, 작성일, 조회수 입력
 			String sql = "INSERT INTO qnas(qna_no, qna_category, user_id, qna_title, qna_content, qna_date, qna_view)"
-					+ " VALUES(seq_qna_no.nextval, ?, ?, ?, ?, sysdate, seq_qna_view.nextval)";
+					+ " VALUES(seq_qna_no.nextval, ?, ?, ?, ?, sysdate, 0)";
 
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 
-			pstmt.setString(1, qcreateDTO.getUser_id());
-			pstmt.setString(2, qcreateDTO.getQna_category());
+			pstmt.setString(1, qcreateDTO.getQna_category());
+			pstmt.setString(2, qcreateDTO.getUser_id());
 			pstmt.setString(3, qcreateDTO.getQna_title());
 			pstmt.setString(4, qcreateDTO.getQna_content());
 
-			pstmt.executeUpdate();
+			rows = pstmt.executeUpdate();
 			pstmt.close();
 
 		} catch (Exception e) {
@@ -87,17 +82,14 @@ public class QnasDAO {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("새글 등록 완료");
+		// 생성 완료되면 1
+		return rows;
 	}
 
 	// QNA 게시판 게시글 수정(QUpdate()-QupdateDTO)
-	public static int QUpdate(QupdateDTO qupdateDTO) {
-
+	public static int QUpdate(QupdateDTO qupdateDTO, Connection conn) {
 		int rows = 0;
 		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@kosa402.iptime.org:50031:orcl", "younghun", "oracle");
-
 			// 카테고리, 제목, 내용 수정
 			// 게시글을 수정하려는 사람이 해당 게시글을 작성한 작성자 본인인지 확인하기 위해 QmatchDAO 필요
 			String sql = "UPDATE qnas SET qna_category = ?, qna_title = ?, qna_content = ?, qna_date = sysdate WHERE qna_no = ?";
@@ -108,32 +100,8 @@ public class QnasDAO {
 			pstmt.setString(2, qupdateDTO.getQna_title());
 			pstmt.setString(3, qupdateDTO.getQna_content());
 			pstmt.setInt(4, qupdateDTO.getQna_no());
+
 			rows = pstmt.executeUpdate();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("글이 수정되었습니다.");
-		// 수정 완료되면 1
-		return rows;
-	}
-
-	// QNA 게시판 게시글 삭제(QDelete()-QdeleteDTO)
-	public static void QDelete(QdeleteDTO qdeleteDTO) {
-		// 게시글을 삭제하려는 사람이 해당 게시글을 작성한 작성자 본인인지 확인하기 위해 QmatchDAO 필요
-		try {
-			String sql = "DELETE FROM qnas WHERE qna_no=?";
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, qdeleteDTO.getQna_no());
-			pstmt.executeUpdate();
 			pstmt.close();
 
 		} catch (Exception e) {
@@ -147,43 +115,57 @@ public class QnasDAO {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("글이 삭제되었습니다.");
+		// 수정 완료되면 1
+		return rows;
+	}
+
+	// QNA 게시판 게시글 삭제(QDelete()-QdeleteDTO)
+	public static int QDelete(QdeleteDTO qdeleteDTO, Connection conn) {
+		int rows = 0;
+		// 게시글을 삭제하려는 사람이 해당 게시글을 작성한 작성자 본인인지 확인하기 위해 QmatchDAO 필요
+		try {
+			String sql = "DELETE FROM qnas WHERE qna_no=?";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, qdeleteDTO.getQna_no());
+
+			rows = pstmt.executeUpdate();
+			pstmt.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		// 삭제 완료되면 1
+		return rows;
 	}
 
 	// QNA 게시판 글 조회(QRead()-QreadDTO)
-	public static void QRead(QreadDTO qreadDTO) {
+	public static QreadDTO QRead(QreadDTO qreadDTO, Connection conn) {
+		QreadDTO qread = new QreadDTO();
 		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@kosa402.iptime.org:50031:orcl", "younghun", "oracle");
-			System.out.println("연결 성공");
-
 			// 전체 게시글 중 글번호 입력해서 상세 내용 조회하기
 			// 게시글을 조회하려는 사람이 해당 게시글을 작성한 작성자 본인인지 확인하기 위해 QmatchDAO 필요
-			String sql = "SELECT * FROM qnas q, users u" + " WHERE q.user_id = u.user_id AND qna_no=?"
-					+ " ORDER BY qna_no DESC";
+			String sql = "SELECT * FROM qnas q, (SELECT user_id FROM users u)u WHERE q.user_id = u.user_id AND qna_no=?";
 
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, qreadDTO.getQna_no());
 			ResultSet rs = pstmt.executeQuery();
 
 			if (rs.next()) {
-				QnasDTO qnas = new QnasDTO();
-				qnas.setQna_no(rs.getInt("qna_no"));
-				qnas.setQna_category(rs.getString("qna_category"));
-				qnas.setUser_id(rs.getString("user_id"));
-				qnas.setQna_title(rs.getString("qna_title"));
-				qnas.setQna_content(rs.getString("qna_content"));
-				qnas.setQna_date(rs.getDate("qna_date"));
-				qnas.setQna_view(rs.getInt("qna_view"));
-				System.out.println("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-				System.out.println("글 번호: " + qnas.getQna_no());
-				System.out.println("문의 유형: " + qnas.getQna_category());
-				System.out.println("제목: " + qnas.getQna_title());
-				System.out.println("내용: " + qnas.getQna_content());
-				System.out.println("작성일: " + qnas.getQna_date());
-				System.out.println("조회수: " + qnas.getQna_view());
-				System.out.println("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+				qread.setQna_no(rs.getInt("qna_no"));
+				qread.setQna_category(rs.getString("qna_category"));
+				qread.setUser_id(rs.getString("user_id"));
+				qread.setQna_title(rs.getString("qna_title"));
+				qread.setQna_content(rs.getString("qna_content"));
+				qread.setQna_date(rs.getDate("qna_date"));
+				qread.setQna_view(rs.getInt("qna_view"));
 			}
 
 			rs.close();
@@ -200,19 +182,13 @@ public class QnasDAO {
 				e.printStackTrace();
 			}
 		}
+		return qread;
 	}
 
 	// QmatchDAO : 게시글 수정/삭제/조회시 원글을 작성한 본인이 맞는지 확인하는 메소드
-	public static boolean Qmatch(String user_id, int qna_no) {
-		Connection conn = null;
+	public static boolean QMatch(String user_id, int qna_no, Connection conn) {
 		boolean isMine = false;
-
 		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@kosa402.iptime.org:50031:orcl", "younghun", "oracle");
-			System.out.println("연결 성공");
-
 			// user_id가 작성한 글 번호들 불러오기
 			String sql = "SELECT qna_no FROM qnas WHERE user_id=?";
 
@@ -250,4 +226,31 @@ public class QnasDAO {
 		return isMine;
 	}
 
+	// QNA 게시판 조회수 카운트 (QViewcount()-QviewcountDTO)
+	public static int QViewcount(QviewcountDTO qviewcountDTO, Connection conn) {
+		int rows = 0;
+		try {
+			// 조회수 +1
+			String sql = "UPDATE qnas SET qna_view = qna_view + 1 WHERE qna_no= ?";
+
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, qviewcountDTO.getQna_no());
+
+			rows = pstmt.executeUpdate();
+			pstmt.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		// 업데이트 되면 1
+		return rows;
+	}
 }
